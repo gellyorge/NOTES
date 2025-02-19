bb06768cfad4284edc3b8bf6fb65b6e9445e5c86
gellyorge <gellyorge23@gmail.com>
2025-02-19 19:40:52 -0300

    alerando notas

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -61,12 +61,50 @@ class MainController extends Controller
 
    public function editNote($id){
       $id = Operation::decryptId($id);
-      
+
       //load note
       $note = Note::find($id);
 
       //shod edit note view
       return view('edit_note', ['note' => $note]);
+   }
+   public function editNoteSubmit(Request $request)
+   {
+      //validade request
+      $request->validate(
+         // rules
+         [
+             'text_title' => 'required|min:3|max:200',
+             'text_note' => 'required|min:3|max:3000'
+         ],
+         // error messages
+         [
+             'text_title.required' => 'O titulo e obrigatorio',
+             'text_note.required' => 'A nota e obrigatorio',
+ 
+              'text_title.min' => 'O titulo deve ter no minimo :min caracteres',
+             'text_title.max' => 'O titulo deve ter no maximo :max caracteres',
+ 
+             'text_note.min' => 'A nota deve ter no minimo :min caracteres',
+             'text_note.max' => 'A nota deve ter no maximo :max caracteres'
+ 
+         ]);
+
+      //check if note id exists
+      if($request->note_id == null){
+         return redirect()->to('/');
+      }
+
+      // decrypt note_id
+      $id = Operation::decryptId($request->note_id);
+      // load note
+      $note = Note::find($id);
+      // update note
+      $note->title = $request->text_title;
+      $note->text = $request->text_note;
+      $note->save();
+      // redirect to home
+      return redirect()->to('/');
    }
 
     
